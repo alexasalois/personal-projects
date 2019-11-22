a167db65c04a3e54dfd9397cfe8f6f91c7bf81dd
Alexandra Salois <alexasalois@hotmail.com>
2019-11-21 21:43:54 -0500

    CART345: added first pages

--- cart345-2019/js/script.js
+++ cart345-2019/js/script.js
@@ -2,10 +2,21 @@
 //
 // by Alexandra Salois
 //
+var nameInput;
 
 function changePage(page) {
   console.log(page);
   $("#page"+page).css('display', 'none');
   page++;
   $("#page"+page).css('display', 'block');
+  console.log(nameInput);
+}
+
+function saveName(name) {
+  nameInput = document.getElementById("name").value;
+//  $("#welcomeCard").css('display','none');
+  $("#welcomeCard").css('display','block');
+  $("#name").css('display','none');
+  $("#inputName").css('display','none');
+  console.log(nameInput);
 }
